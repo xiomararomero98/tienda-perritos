abd91c616e92c13cad2dc1d3a7d57cc904323928
jordyhrnlzn <101402897+jordyhrnlzn@users.noreply.github.com>
2026-05-22 09:02:23 -0400

    new ip back

--- frontend/app.js
+++ frontend/app.js
@@ -9,7 +9,7 @@ const API_BASE = (() => {
     return "http://localhost:3001/api/productos";
   }
   // Para ejecución en EC2 u otro host, usar el mismo hostname y puerto 3001
-  return `http://18.232.93.28:3001/api/productos`;
+  return `http://13.221.176.34:3001/api/productos`;;
 })();
 
 let editandoId = null;
